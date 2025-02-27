a05c28d4022cdc27bfc9700dfbc76622ab662603
Pranav Narayan Chandaa Soneji <pranav1544@tsecol.onmicrosoft.com>
2025-02-27 20:31:44 +0530

    Update home_page.dart

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -8,8 +8,21 @@ class HomeUI extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(  title: Text('Currency Converter'),
-          flexibleSpace: Container(
+        // appBar: AppBar(  title: Text('Currency Converter'),
+        //   flexibleSpace: Container(
+        //     decoration: BoxDecoration(
+        //       gradient: LinearGradient(
+        //         colors: [Color.fromRGBO(135, 206, 235, 1), Color.fromRGBO(245, 239, 224, 1)], // Gradient colors
+        //         begin: Alignment.topLeft,
+        //         end: Alignment.bottomRight,
+        //       ),
+        //     ),
+        //   ),
+        // ),
+      body:
+        Container(
+          width: double.infinity,
+          height: double.infinity,
             decoration: BoxDecoration(
               gradient: LinearGradient(
                 colors: [Color.fromRGBO(135, 206, 235, 1), Color.fromRGBO(245, 239, 224, 1)], // Gradient colors
@@ -17,28 +30,26 @@ class HomeUI extends StatelessWidget {
                 end: Alignment.bottomRight,
               ),
             ),
-          ),
-        ),
-      body:
-        Center(
-          child:Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text("WELCOME TO CURRENCY CONVERTER!", textAlign: TextAlign.center, style: TextStyle(fontSize: 50, color: Color.fromRGBO(139, 69, 19, 1), fontWeight: FontWeight.bold),),
-              Container(
-                padding: EdgeInsets.all(20),
-                margin: EdgeInsets.all(10),
-                child: ElevatedButton(onPressed: (){
-                  Navigator.push(context, MaterialPageRoute(builder: (context)=> Converter()));
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.white,
-                    padding: EdgeInsets.only(left:80, right: 80, top: 5, bottom: 5),
+          child: Center(
+            child:Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("WELCOME TO CURRENCY CONVERTER!", textAlign: TextAlign.center, style: TextStyle(fontSize: 50, color: Color.fromRGBO(139, 69, 19, 1), fontWeight: FontWeight.bold),),
+                Container(
+                  padding: EdgeInsets.all(20),
+                  margin: EdgeInsets.all(10),
+                  child: ElevatedButton(onPressed: (){
+                    Navigator.push(context, MaterialPageRoute(builder: (context)=> Converter()));
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      padding: EdgeInsets.only(left:80, right: 80, top: 5, bottom: 5),
+                    ),
+                    child: Text("Continue", style: TextStyle(color: Colors.black, fontSize: 30)),
                   ),
-                  child: Text("Continue", style: TextStyle(color: Colors.black, fontSize: 30)),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         )
 
