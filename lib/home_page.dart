ba3f10a39b9afda8bd3f2a3de9e9f8f704a65796
Pranav Narayan Chandaa Soneji <pranav1544@tsecol.onmicrosoft.com>
2025-02-27 18:40:01 +0530

    Update home_page.dart

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:currency/io.dart';
 import 'package:flutter/material.dart';
 import 'package:currency/convertor.dart';
 
@@ -8,11 +9,20 @@ class HomeUI extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.dark(),
+      title: "Hello",
+      theme: ThemeData(scaffoldBackgroundColor: const Color.fromRGBO(245, 239, 224, 1),
+      ),
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(title: Text("Currency Convertor", textAlign: TextAlign.center,
-          style: TextStyle(fontSize: 35),
+        appBar: AppBar(  title: Text('Currency Convertor'),
+          flexibleSpace: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Color.fromRGBO(135, 206, 235, 1), Color.fromRGBO(245, 239, 224, 1)], // Gradient colors
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+            ),
           ),
         ),
       body:
@@ -20,12 +30,19 @@ class HomeUI extends StatelessWidget {
           child:Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text("WELCOME TO CURRENCY CONVERTOR!", textAlign: TextAlign.center, style: TextStyle(fontSize: 50, color: Colors.teal),),
-              ElevatedButton(onPressed: (){
-                Navigator.push(context, MaterialPageRoute(builder: (context) => Convertor()),
-                );
-                },
-                child: Text("Continue"),
+              Text("WELCOME TO CURRENCY CONVERTOR!", textAlign: TextAlign.center, style: TextStyle(fontSize: 50, color: Color.fromRGBO(139, 69, 19, 1), fontWeight: FontWeight.bold),),
+              Container(
+                padding: EdgeInsets.all(20),
+                margin: EdgeInsets.all(10),
+                child: ElevatedButton(onPressed: (){
+                  Navigator.push(context, MaterialPageRoute(builder: (context)=> Hello()));
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    padding: EdgeInsets.only(left:80, right: 80, top: 5, bottom: 5),
+                  ),
+                  child: Text("Continue", style: TextStyle(color: Colors.black, fontSize: 30)),
+                ),
               ),
             ],
           ),
