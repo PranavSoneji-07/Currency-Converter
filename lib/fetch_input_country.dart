88d169a31001b3c437e37f0e31a176ffbe52e7f5
Pranav Narayan Chandaa Soneji <pranav1544@tsecol.onmicrosoft.com>
2025-03-02 12:18:52 +0530

    Update fetch_input_country.dart

--- lib/fetch_input_country.dart
+++ lib/fetch_input_country.dart
@@ -30,7 +30,7 @@ Future<void> FetchJapan() async{
 }
 
 Future<void> FetchSouthKorea() async{
-  final url = Uri.https('v6.exchangerate-api.com', 'v6/36540c75dc2dbfd0599b22a0/latest/USD');
+  final url = Uri.https('v6.exchangerate-api.com', 'v6/36540c75dc2dbfd0599b22a0/latest/KWR');
   var response = await http.get(url);
   final data = jsonDecode(response.body);
 
