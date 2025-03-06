b2fb9f6f140f14bfe5971c7dce4ff0855b637a4f
Pranav Narayan Chandaa Soneji <pranav1544@tsecol.onmicrosoft.com>
2025-03-06 23:24:54 +0530

    Huge upgrades in the design and theme

--- lib/converter_page.dart
+++ lib/converter_page.dart
@@ -25,101 +25,65 @@ class _Currency extends State<Converter> {
   Widget build(BuildContext context) {
     return Scaffold(
     extendBodyBehindAppBar: true,
-      appBar: AppBar(  title: Text('Currency Converter', style: TextStyle(color: Colors.black), textAlign: TextAlign.center,),
-    ),
-      body: Container(
-        width: double.infinity,
-        height: double.infinity,
-        margin: EdgeInsets.only(top: 112),
-
-        decoration: BoxDecoration(
-          color: Color.fromRGBO(156, 161, 209, 0.5),
-          borderRadius: BorderRadius.circular(30),
-        ),
-        child: Column(
-         crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Container(
-              height: 80,
-              padding: EdgeInsets.all(20),
-              decoration: BoxDecoration(
-                color: Color.fromRGBO(250, 250, 250, 1),
-                borderRadius: BorderRadius.circular(20)
-              ),
-              margin: EdgeInsets.only(left: 25, right: 25, top: 180),
-              child:
-
-              //Input
-              TextField(
-                controller: controller,
-                keyboardType: TextInputType.number,
-                inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-                decoration: InputDecoration(
-                  border: InputBorder.none,
-                  hintText: "Enter amount",
-                ),
-                onChanged: (value) async {
-                  setState(() {
-                    if(value=="" || value == " "){
-                      textval =0;
-                    }
-                    else
-                    {textval = double.parse(value);}
-                    Output = fetch.out(textval, selectedValue1);
-                  });
-                },
-              ),
+      appBar: AppBar(  title: Text('Converter', style: TextStyle(color: Colors.black), textAlign: TextAlign.center),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Container(
+              margin: EdgeInsets.all(5),
+              child: Text("Amount", style: TextStyle(color: Colors.black),)),
+          Container(
+            height: 80,
+            padding: EdgeInsets.all(20),
+            decoration: BoxDecoration(
+              color: Color.fromRGBO(250, 250, 250, 1),
+              borderRadius: BorderRadius.circular(20)
             ),
+            margin: EdgeInsets.only(left: 25, right: 25, top: 150),
+            child:
+
+            //Input
+            TextField(
+              controller: controller,
+              keyboardType: TextInputType.number,
+              inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                hintText: "Enter amount",
+              ),
+              onChanged: (value) async {
+                setState(() {
+                  if(value=="" || value == " "){
+                    textval = 0;
+                  }
+                  else
+                  {textval = double.parse(value);}
+                  Output = fetch.out(textval, selectedValue1);
+                });
+              },
+            ),
+          ),
 
 
 
-            // Dropdown Menus
-            Row(
-              children: [
-                Container(
-                  width: 145,
-                  height: 130,
-                  margin: EdgeInsets.only(left: 10, top: 30),
-                  child:
-
-                  //Input Currency
-                  DropdownMenu(
-                    textStyle: TextStyle(fontSize: 20,),
-                    onSelected: (value) {
-                      selectedValue = value.toString();
-                      fetch.fetchData(selectedValue);
-                      setState(() {
-                        Output = fetch.out(textval, selectedValue1);
-                      });
-                    },
-                    initialSelection: selectedValue,
-                    dropdownMenuEntries: [
-                      DropdownMenuEntry(value: "INR", label: "Indian ruppee"),
-                      DropdownMenuEntry(value: "USD", label: "US Dollar"),
-                      DropdownMenuEntry(value: "JPY", label: "Japanese Yen"),
-                      DropdownMenuEntry(value: "AED", label: "UAE Dirham"),
-                      DropdownMenuEntry(value: "GBP", label: "Pound Sterling"),
-                      DropdownMenuEntry(value: "KRW", label: "South Korean Won"),
-                      DropdownMenuEntry(value: "CNY", label: "Chinese Renminbi"),
-                      DropdownMenuEntry(value: "CAD", label: "Canadian Dollar"),
-                    ],
-                    width: 145,
-                  ),
+          // Dropdown Menus
+          Row(
+            children: [
+              Container(
+                padding: EdgeInsets.only(left: 10, top: 5, right: 5, bottom: 5),
+                margin: EdgeInsets.only(left: 10, bottom: 38, top: 90),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Color.fromRGBO(89, 100, 236, 1),
                 ),
-                Container(
-                  height: 54,
-                  margin: EdgeInsets.only(left: 80, bottom: 38),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Color.fromRGBO(149, 147, 224, 0.8),
-                  ),
-                  child:
+                child:
 
 
-                  //Output Currency
+                //Input Currency
 
-                  DropdownMenu(
-                  textStyle: TextStyle(fontSize: 20,),
+                DropdownMenu(
+                  textStyle: TextStyle(fontSize: 30, color: Colors.white),
                   onSelected: (value) {
                     selectedValue1 = value.toString();
                     fetch.fetchOut(selectedValue1);
@@ -138,26 +102,63 @@ class _Currency extends State<Converter> {
                     DropdownMenuEntry(value: "CNY", label: "Chinese Renminbi"),
                     DropdownMenuEntry(value: "CAD", label: "Canadian Dollar"),
                   ],
-                  width: 145,
-                    inputDecorationTheme: InputDecorationTheme(
-                      outlineBorder: BorderSide.none,
-                    ),
+                  width: 150,
+                  inputDecorationTheme: InputDecorationTheme(
+                    outlineBorder: BorderSide.none,
+                  ),
                 ),
-                ),
-              ],
-            ),
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(100),
-                color: Color.fromRGBO(250, 250, 250, 1),
               ),
-              width: double.infinity,
-              height: 90,
-              margin: EdgeInsets.only(left: 70, right: 70, top: 30),
-              child: Center(child: Text(Output.toString(), style: TextStyle(fontSize: 40, color: Colors.black), textAlign: TextAlign.center,)),
+              Container(
+                padding: EdgeInsets.only(left: 10, top: 5, right: 5, bottom: 5),
+                margin: EdgeInsets.only(left: 55, bottom: 38, top: 90),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Color.fromRGBO(89, 100, 236, 1),
+                ),
+                child:
+
+
+                //Output Currency
+
+                DropdownMenu(
+                textStyle: TextStyle(fontSize: 30, color: Colors.white,),
+                onSelected: (value) {
+                  selectedValue1 = value.toString();
+                  fetch.fetchOut(selectedValue1);
+                  setState(() {
+                    Output = fetch.out(textval, selectedValue1);
+                  });
+                },
+                initialSelection: selectedValue1,
+                dropdownMenuEntries: [
+                  DropdownMenuEntry(value: "INR", label: "Indian ruppee"),
+                  DropdownMenuEntry(value: "USD", label: "US Dollar"),
+                  DropdownMenuEntry(value: "JPY", label: "Japanese Yen"),
+                  DropdownMenuEntry(value: "AED", label: "UAE Dirham"),
+                  DropdownMenuEntry(value: "GBP", label: "Pound Sterling"),
+                  DropdownMenuEntry(value: "KRW", label: "South Korean Won"),
+                  DropdownMenuEntry(value: "CNY", label: "Chinese Renminbi"),
+                  DropdownMenuEntry(value: "CAD", label: "Canadian Dollar"),
+                ],
+                width: 150,
+                  inputDecorationTheme: InputDecorationTheme(
+                    outlineBorder: BorderSide.none,
+                  ),
+              ),
+              ),
+            ],
+          ),
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(100),
+              color: Color.fromRGBO(250, 250, 250, 1),
             ),
-          ],
-        ),
+            width: double.infinity,
+            height: 90,
+            margin: EdgeInsets.only(left: 70, right: 70, top: 69),
+            child: Center(child: Text(Output.toString(), style: TextStyle(fontSize: 40, color: Colors.black), textAlign: TextAlign.center,)),
+          ),
+        ],
       ),
 
     );
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -23,29 +23,45 @@ class HomeUI extends StatelessWidget {
         Container(
           width: double.infinity,
           height: double.infinity,
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                colors: [Color.fromRGBO(135, 206, 235, 1), Color.fromRGBO(245, 239, 224, 1)], // Gradient colors
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-              ),
-            ),
+          padding: EdgeInsets.zero,
+          color: Colors.white,
           child: Center(
             child:Column(
+
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Text("WELCOME TO CURRENCY CONVERTER!", textAlign: TextAlign.center, style: TextStyle(fontSize: 50, color: Color.fromRGBO(139, 69, 19, 1), fontWeight: FontWeight.bold),),
+                Image.asset("assets/Globe.png", width: 300, height: 380,),
+                Text("MoneyMate", textAlign: TextAlign.center, style: TextStyle(fontSize: 45, color: Color.fromRGBO(0,0,0,1), fontWeight: FontWeight.w900),),
                 Container(
                   padding: EdgeInsets.all(20),
-                  margin: EdgeInsets.all(10),
-                  child: ElevatedButton(onPressed: (){
-                    Navigator.push(context, MaterialPageRoute(builder: (context)=> Converter()));
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.white,
-                      padding: EdgeInsets.only(left:80, right: 80, top: 5, bottom: 5),
+                  margin: EdgeInsets.only(bottom: 60),
+                  child: Container(
+                    margin: EdgeInsets.only(top: 20),
+                    decoration: BoxDecoration(
+                      boxShadow: [BoxShadow(
+                        color: Colors.black.withOpacity(0.29),
+                        blurRadius: 25,
+                        spreadRadius: 2,
+                        offset: Offset(0, 5),
+                      ),
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.2), // Lighter shadow for fade effect
+                          blurRadius: 35,
+                          spreadRadius: 0,
+                          offset: Offset(0, 10), // Extending shadow downward
+                        ),
+
+                      ]
                     ),
-                    child: Text("Continue", style: TextStyle(color: Colors.black, fontSize: 30)),
+                    child: ElevatedButton(onPressed: (){
+                      Navigator.push(context, MaterialPageRoute(builder: (context)=> Converter()));
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.white,
+                        padding: EdgeInsets.only(left:80, right: 80, top: 10, bottom: 10),
+                      ),
+                      child: Text("Get Started", style: TextStyle(color: Colors.black, fontSize: 32)),
+                    ),
                   ),
                 ),
               ],
